3f216de286f779348d389076f64dc939af3b2982
Floris Luiten <bitbucket@florisluiten.nl>
2022-06-26 21:55:59 +0200

    Introduced helper for bitwise operations

---
+++ helpers/calculatable.php
@@ -0,0 +1,26 @@
+#!/usr/bin/env php
+<?php
+
+/**
+ * List all characters that can be created with some arithmetics
+ */
+
+$characters = [];
+
+foreach (glob('*.php') as $filename) {
+    $characters[] = basename($filename, '.php');
+}
+
+foreach ($characters as $a) {
+    foreach ($characters as $b) {
+        if ($a == $b) {
+            continue;
+        }
+
+        $calulated = $a ^ $b;
+
+        if (!in_array($calulated, $characters) && ctype_alnum($calulated)) {
+            echo "$a ^ $b = $calulated\n";
+        }
+    }
+}
